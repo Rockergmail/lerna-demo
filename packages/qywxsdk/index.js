d6bdc049ffb5693351647fbc0ae1e795ce591102
xiangrong.liu_刘祥荣 <xiangrong.liu@wehotelglobal.com>
2022-06-09 16:27:35 +0800

    feat: donbe

---
+++ packages/qywxsdk/index.js
@@ -0,0 +1,10 @@
+/*
+ * @description: 
+ * @author: xiangrong.liu
+ * @Date: 2022-06-09 16:26:28
+ * @LastEditors: xiangrong.liu
+ * @LastEditTime: 2022-06-09 16:26:35
+ */
+export default {
+    fuck: 1
+}
